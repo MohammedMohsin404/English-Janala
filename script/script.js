078da2ad6a3048173ebb8c320d03760d718f2dd0
MohammedMohsin404 <mohammedmohsin0725@gmail.com>
2025-06-22 00:44:26 +0600

    Dynamicaly Button Complete

--- script/script.js
+++ script/script.js
@@ -1,11 +1,33 @@
 console.log("JS Connected");
-const lessons =["Lesson-1", "Lesson-2","Lesson-3","Lesson-4","Lesson-5","Lesson-6","Lesson-7"]
-const lessons_Container=document.getElementById("lessons-container")
-lessons.forEach((lesson)=>{
-    
-lessons_Container.innerHTML+=`
-<button class=" btn border hover:border-[#422AD5] text-[#422AD5] bg-transparent"><img src="../assets/fa-book-open.png" alt="${lesson}" /> ${lesson}</button>
-`
-})
 
+const lessons_Container = document.getElementById("lessons-container");
 
+const Load_Lesson_Buttons = async () => {
+  try {
+    const response = await fetch(
+      "https://openapi.programming-hero.com/api/levels/all"
+    );
+
+    // Optional: handle HTTP errors manually
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json(); // or .text(), .blob(), etc.
+    console.log("Fetched Data:", data);
+    Show_Lesson_Buttons(data.data);
+  } catch (error) {
+    console.error("Fetch failed:", error.message);
+  }
+};
+
+const Show_Lesson_Buttons = (lessons) => {
+  lessons.forEach((lesson) => {
+
+    lessons_Container.innerHTML += `
+<button class=" btn border hover:border-[#422AD5] text-[#422AD5] bg-transparent"><img src="../assets/fa-book-open.png" alt="${lesson.lessonName}" /> Lesson-${lesson.level_no}</button>
+`;
+  });
+};
+
+Load_Lesson_Buttons();
